2c09e5cbc91fa38e02466773186c629f1621f099
CandelaAlfaroFontana <candealfaro18@gmail.com>
2024-05-27 21:50:04 -0300

    commit

---
+++ Js/Index.js
@@ -0,0 +1,29 @@
+
+let USUARIO = prompt("Ingrese su mail registrado");
+let CONTRASENA = prompt("Ingrese su contrasena");
+
+let Antiguedad;
+let AntiguedadMinima;
+let anoValido = false;
+
+
+while (!anoValido) {
+    Antiguedad = prompt("Ingresa el año de tu vehiculo");
+    console.log(Antiguedad);
+
+    AntiguedadMinima = parseInt(Antiguedad);
+
+
+    if (isNaN(AntiguedadMinima) || Antiguedad.length !== 4 || AntiguedadMinima > new Date().getFullYear()) {
+        alert("Por favor, ingresa un año válido para tu vehículo (4 cifras y menor o igual al año actual).");
+    } else {
+        anoValido = true; 
+    }
+}
+
+
+if (AntiguedadMinima >= 2003) {
+    console.log("Antigüedad de vehículo autorizada para cotizar");
+} else if (AntiguedadMinima < 2003) {
+    console.log("Disculpe, vehículo fuera de pauta para asegurar, comuníquese con atención al cliente");
+}
